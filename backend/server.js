d2e1513ca3f8cec07096ee5c9bc157dda371d736
Priyanshu Sidar <sidarsunita2@gmail.com>
2025-01-12 21:30:22 +0530

    updating backend and enableing cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-  useNewUrlParser: true,
-});
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 const User = mongoose.model(
   "user",
@@ -22,11 +21,18 @@ const Drill = mongoose.model(
 );
 const UserDrill = mongoose.model(
   "user_drill",
-  new mongoose.Schema({ userId: String, drillId: String, count: Number })
+  new mongoose.Schema({
+    userId: String,
+    drillId: String,
+    drill_name: String,
+    count: Number,
+    drill_total: Number,
+  })
 );
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Signup API
 app.post("/api/signup", async (req, res) => {
@@ -56,15 +62,57 @@ app.get("/api/drills", async (req, res) => {
 // Submit Drill Count
 app.post("/api/submit-drill", async (req, res) => {
   const { userId, drillId, count } = req.body;
-  const drill = await Drill.findById(drillId);
-  if (count <= drill.total_count) {
-    await UserDrill.create({ userId, drillId, count });
-    const user = await User.findById(userId);
-    user.totalCount += count;
-    await user.save();
-    res.send({ success: true });
-  } else {
-    res.send({ success: false });
+  try {
+    const drill = await Drill.findById(drillId);
+    if (!drill) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Drill not found" });
+    }
+    const drill_name = drill.name;
+    const drill_total = drill.total_count;
+
+    const userDrill = await UserDrill.findOne({ userId, drillId });
+    if (!userDrill) {
+      // If the drill is not yet registered for the user
+      if (count <= drill.total_count) {
+        await UserDrill.create({
+          userId,
+          drillId,
+          drill_name,
+          count,
+          drill_total,
+        });
+        const user = await User.findById(userId);
+        user.totalCount += count;
+        await user.save();
+        return res.send({ success: true });
+      } else {
+        return res.send({
+          success: false,
+          message: "Count exceeds the drill's total count",
+        });
+      }
+    } else {
+      // If the drill is already registered for the user
+      const totalRegisteredCount = userDrill.count + count;
+      if (totalRegisteredCount <= drill.total_count) {
+        userDrill.count = totalRegisteredCount;
+        await userDrill.save();
+        const user = await User.findById(userId);
+        user.totalCount += count;
+        await user.save();
+        return res.send({ success: true });
+      } else {
+        return res.send({
+          success: false,
+          message: "Total count exceeds the drill's total count",
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ success: false, message: "Internal server error" });
   }
 });
 
